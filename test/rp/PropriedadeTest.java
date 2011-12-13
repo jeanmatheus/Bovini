44e79125f3718db9c47d842bb669f0be0e5e82a4
jeanmatheus <jeanmatheus@jeanmatheus-note>
2011-12-13 00:40:55 -0200

    Arrumado arquivo de Teste, para fazer teste melhores

--- test/rp/PropriedadeTest.java
+++ test/rp/PropriedadeTest.java
@@ -6,7 +6,10 @@ package rp;
 
 import br.com.jean.faculdade.projeto.model.Propriedade;
 import br.com.jean.faculdade.projeto.repositorio.PropriedadeRP;
-import java.util.Date;
+
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -14,31 +17,25 @@ import org.junit.Test;
  * @author adml01
  */
 public class PropriedadeTest {
+
+    Propriedade pro;
+    PropriedadeRP proRP;
+    
+    @Before
+    public void PropriedadeTest() {
+        pro = new Propriedade();
+        proRP = new PropriedadeRP();
+    } 
     
     @Test
     public void inserirPropriedade(){
-        
-        PropriedadeRP rp = new PropriedadeRP();
-        
-        for(int i = 0; i < 5; i++){
-            Propriedade p = new Propriedade();
-            p.setBlocoProdutor(Long.valueOf(123 * i));
-            p.setComplemento("TESTES "+i);
-            p.setNome("TESTE NOME "+i+1*3);
-            p.setNomeFantasia("TESTE NOME FANTASIA "+i*2);
-            p.setDataCadastro(new Date());
-            
-            rp.salvar(p);
-        }
+        //REFAZER TESTE DE INSERÇÃO DE PROPRIEDADE
     }
     
-    /*
+    
     @Test
-    public void removerTest(){
-        PropriedadeRP rp = new PropriedadeRP();
-        
-        rp.remover(10l);
-        
+    public void removerTest(){ 
+       //FAZER TESTE DE REMOÇÃO
     }
-    */
+    
 }
