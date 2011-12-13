04332ff852de60c21d4fb78a3e3ecfcd089a1777
jeanmatheus <jeanmatheus@jeanmatheus-note>
2011-12-13 00:40:47 -0200

    Arrumado arquivo de Teste, para fazer teste melhores

--- test/rp/PessoaRPTest.java
+++ test/rp/PessoaRPTest.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -25,83 +26,80 @@ import org.junit.Test;
  * @author adml01
  */
 public class PessoaRPTest {
-   
-    /*
+
+    Pessoa pessoa;
+    PessoaRP pessoaRP;
+
+    @Before
+    public void pessoaTest() {
+        pessoa = new Pessoa();
+        pessoaRP = new PessoaRP();
+    }
+
     @Test
-    public void insere10Test(){
-        PessoaRP r = new PessoaRP();
-        
-        for(int i = 0; i< 10;i++){
-            Pessoa p = new Pessoa();
-            p.setDataCadastro(new Date());
-            p.setEmail("email"+i);
-            p.setNome("nome"+i);
-            
-            r.salvar(p);
+    public void addAdmin() {
+
+        if (pessoaRP.getPorNome("admin") == null) {
+
+            String senha = "admin";
+            pessoa.setCpf(123456789l);
+            pessoa.setDataCadastro(new Date());
+            pessoa.setEmail("admin@admin.com.br");
+            pessoa.setNome("admin");
+            pessoa.setSenha(senha.hashCode());
+
+            Pais pais = new Pais();
+            pais.setNome("Brasil");
+
+            PaisRP rpPais = new PaisRP();
+
+            rpPais.salvar(pais);
+
+            Uf uf = new Uf();
+            uf.setNome("Santa Catarina");
+            uf.setPais(rpPais.getPorNome("Brasil"));
+            uf.setSigla("SC");
+
+            UfRP rpUF = new UfRP();
+            rpUF.salvar(uf);
+
+            Cidade ci = new Cidade();
+            ci.setCep(89820000l);
+            ci.setNome("Xanxere");
+            ci.setUf(rpUF.getPorNome("Santa Catarina"));
+
+            CidadeRP rpCidade = new CidadeRP();
+            rpCidade.salvar(ci);
+
+            Endereco r = new Endereco();
+            r.setNumero(123);
+            r.setRua("rua do endereço");
+            r.setCidade(rpCidade.getPorNome("Xanxere"));
+
+            EnderecoRP rpEndereco = new EnderecoRP();
+            rpEndereco.salvar(r);
+
+            pessoa.setEndereco(r);
+
+            pessoaRP.salvar(pessoa);
+
+
         }
+        
+        Assert.assertEquals("admin", pessoaRP.getPorNome("admin").getNome());
     }
-     */
-    
+
     @Test
-    public void addAdmin(){
-     Pessoa p = new Pessoa();
-     String senha = "admin";
-     p.setCpf(123456789l);
-     p.setDataCadastro(new Date());
-     p.setEmail("admin@admin.com.br");
-     p.setNome("admin");
-     p.setSenha(senha.hashCode());
-     
-     Pais pais = new Pais();
-     pais.setNome("Brasil");
-     
-     PaisRP rpPais = new PaisRP();
-     
-     rpPais.salvar(pais);
-     
-     Uf uf = new Uf();
-     uf.setNome("Santa Catarina");
-     uf.setPais(rpPais.getPorNome("Brasil"));
-     uf.setSigla("SC");
-     
-     UfRP rpUF = new UfRP();
-     rpUF.salvar(uf);
-     
-     Cidade ci = new Cidade();
-     ci.setCep(89820000l);
-     ci.setNome("Xanxere");
-     ci.setUf(rpUF.getPorNome("Santa Catarina"));
-     
-     CidadeRP rpCidade = new CidadeRP();
-     rpCidade.salvar(ci);
-     
-     Endereco r = new Endereco();
-     r.setNumero(123);
-     r.setRua("rua do endereço");
-     r.setCidade(rpCidade.getPorNome("Xanxere"));
-     
-     EnderecoRP rpEndereco = new EnderecoRP();
-     rpEndereco.salvar(r);
-     
-     p.setEndereco(r);
-     
-     PessoaRP  rpPessoa = new PessoaRP();
-     rpPessoa.salvar(p);
-    }
-    
-    @Test
-    public void buscaTodosTest(){
+    public void buscaTodosTest() {
         PessoaRP rp = new PessoaRP();
         //System.out.println("teste2");
-        
+
         List<Pessoa> lista = new ArrayList<Pessoa>();
-        
+
         lista.addAll(rp.getTodos());
-        
-        for (Pessoa pessoa : lista) {
-            System.out.println(""+pessoa.toString());
+
+        for (Pessoa pes : lista) {
+            System.out.println("" + pes.toString());
         }
     }
-    
-
 }
